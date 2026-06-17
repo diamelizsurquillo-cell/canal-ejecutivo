bb38acba9b865646e018b17800334a121ebfaefb
Jefferson <jefferson_15_6@hotmail.com>
2026-06-17 17:07:20 -0500

    fix: revertir a valores hardcodeados de Supabase para que funcione en Vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://pycxrxippgwquyjcnkwf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5Y3hyeGlwcGd3cXV5amNua3dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3OTgwODIsImV4cCI6MjA5NDM3NDA4Mn0.EdrGUrDPw69RKtbNeC7BO6kwwe7rx1FHUpFUlakeItg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
